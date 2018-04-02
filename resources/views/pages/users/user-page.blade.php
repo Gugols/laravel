1aea34af3d51a0b34f336a4b2b503e809c5923bd
Dāvis Are <davis.are@wunder.io>
2018-04-02 16:31:14 +0300

    Changed header class

--- resources/views/pages/users/user-page.blade.php
+++ resources/views/pages/users/user-page.blade.php
@@ -20,78 +20,9 @@
     <!-- Wrapper -->
     <div id="wrapper">
 
-        <!-- Topbar -->
-        <div id="topbar" class="topbar-transparent topbar-fullwidth dark visible-md visible-lg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <ul class="top-menu">
-                            <li>
-                                <a href="#">Phone: +371 27102912</a>
-                            </li>
-                            <li>
-                                <a href="#">Email: info@maecenas.test</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-6 hidden-xs">
-                        <div class="social-icons social-icons-colored-hover">
-                            <ul>
-                                <li class="social-facebook">
-                                    <a href="#">
-                                        <i class="fa fa-facebook"></i>
-                                    </a>
-                                </li>
-                                <li class="social-twitter">
-                                    <a href="#">
-                                        <i class="fa fa-twitter"></i>
-                                    </a>
-                                </li>
-                                <li class="social-google">
-                                    <a href="#">
-                                        <i class="fa fa-google-plus"></i>
-                                    </a>
-                                </li>
-                                <li class="social-pinterest">
-                                    <a href="#">
-                                        <i class="fa fa-pinterest"></i>
-                                    </a>
-                                </li>
-                                <li class="social-vimeo">
-                                    <a href="#">
-                                        <i class="fa fa-vimeo-square"></i>
-                                    </a>
-                                </li>
-                                <li class="social-linkedin">
-                                    <a href="#">
-                                        <i class="fa fa-linkedin"></i>
-                                    </a>
-                                </li>
-                                <li class="social-dribbble">
-                                    <a href="#">
-                                        <i class="fa fa-dribbble"></i>
-                                    </a>
-                                </li>
-                                <li class="social-youtube">
-                                    <a href="#">
-                                        <i class="fa fa-youtube-play"></i>
-                                    </a>
-                                </li>
-                                <li class="social-rss">
-                                    <a href="#">
-                                        <i class="fa fa-rss"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end: Topbar -->
 
         <!-- Header -->
-        <header id="header" class="header-fullwidth header-transparent dark">
+        <header id="header" class="header-transparent dark">
             <div id="header-wrap">
                 <div class="container">
                     <!--Logo-->
